2ba516d4b192072f204b0d2191de06669455db91
Cao Nguyen Thanh An <caonguyenthanhan@gmail.com>
2024-10-29 15:15:21 +0700

    thêm bài demo4

---
+++ src/test/java/vn/iotstar/controller/CategoryController.java
@@ -0,0 +1,22 @@
+package vn.iotstar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import vn.iotstar.entity.Category;
+import vn.iotstar.service.CategoryService;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public String listCategories(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "category/list"; 
+    }
+}
---
+++ src/test/java/vn/iotstar/entity/Category.java
@@ -0,0 +1,23 @@
+package vn.iotstar.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    // Các thuộc tính khác của Category (nếu có)
+}
